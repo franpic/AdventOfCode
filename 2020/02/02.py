8e7ea65220a312738bc816d09d5309e4ae655a1a
franpic <franpic@live.it>
2021-01-06 18:14:51 +0100

    Day 2 - Part Two Done

--- 2020/02/02.py
+++ 2020/02/02.py
@@ -1,20 +1,38 @@
 # -*- coding: utf-8 -*-
 
-from time import process_time
 import re
+
+pattern_to_parse_password_policies = re.compile(
+    r"(\d+)-(\d+) (\w): (\w+)")
+
+
+def get_part_one_valid_password_count(input_data: list[str]):
+    valid_passwords_count = 0
+    for line in input_data:
+        min_instancies_allowed, max_instancies_allowed, character, password = \
+            pattern_to_parse_password_policies.match(line).groups()
+        if \
+                int(min_instancies_allowed) <= \
+                password.count(character) <= \
+                int(max_instancies_allowed):
+            valid_passwords_count = valid_passwords_count + 1
+    return valid_passwords_count
+
+
+def get_part_two_valid_password_count(input_data: list[str]):
+    valid_passwords_count = 0
+    for line in input_data:
+        first_index, second_index, character, password = \
+            pattern_to_parse_password_policies.match(line).groups()
+        indexes_to_check = (int(first_index) - 1, int(second_index) - 1)
+        characters_to_check = tuple(password[i] for i in indexes_to_check)
+        if characters_to_check.count(character) == 1:
+            valid_passwords_count = valid_passwords_count + 1
+    return valid_passwords_count
+
 
 if __name__ == "__main__":
     with open("data02.txt") as f:
-        pattern_to_parse_password_policies = re.compile(
-            r"(\d+)-(\d+) (\w): (\w+)")
-        valid_passwords_count = 0
-        for line in f:
-            min_instancies_allowed, max_instancies_allowed, character, password\
-                = pattern_to_parse_password_policies.match(line).groups()
-            # print(pattern_to_parse_password_policies.match(line).groups())
-            if \
-                    int(min_instancies_allowed) <= \
-                    password.count(character) <= \
-                    int(max_instancies_allowed):
-                valid_passwords_count = valid_passwords_count + 1
-        print(valid_passwords_count)
+        data = f.readlines()
+    print(get_part_one_valid_password_count(input_data=data))
+    print(get_part_two_valid_password_count(input_data=data))
