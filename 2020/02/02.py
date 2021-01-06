cb77a292a218b2d1a52606065e5c8962520259eb
franpic <franpic@live.it>
2021-01-06 17:37:30 +0100

    Day 2 - Part One Done

--- 2020/02/02.py
+++ 2020/02/02.py
@@ -4,9 +4,17 @@ from time import process_time
 import re
 
 if __name__ == "__main__":
-    with open("data01.txt") as f:
-        data = tuple(int(line.strip()) for line in f)
-    regex_to_parse_data = r"(\d+)-(\d+) (\w): (\w+)"
-    re.compile(regex_to_parse_data)
-    
-    print(len())
+    with open("data02.txt") as f:
+        pattern_to_parse_password_policies = re.compile(
+            r"(\d+)-(\d+) (\w): (\w+)")
+        valid_passwords_count = 0
+        for line in f:
+            min_instancies_allowed, max_instancies_allowed, character, password\
+                = pattern_to_parse_password_policies.match(line).groups()
+            # print(pattern_to_parse_password_policies.match(line).groups())
+            if \
+                    int(min_instancies_allowed) <= \
+                    password.count(character) <= \
+                    int(max_instancies_allowed):
+                valid_passwords_count = valid_passwords_count + 1
+        print(valid_passwords_count)
